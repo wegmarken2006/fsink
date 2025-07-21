a8a30a4a4189db8243db0e95d4e4abf85a8b32e4
gb <gigi@esempio.com>
2025-07-21 14:21:37 +0200

    andorid audio no gestures

--- lib/utils_webview.dart
+++ lib/utils_webview.dart
@@ -1,20 +1,24 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
-
-
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 WebViewController uInitWebview() {
-  var controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  var controller =
+      WebViewController()..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+  //var params = WebKitWebViewControllerCreationParams();
+
+  if (controller.platform is AndroidWebViewController) {
+    AndroidWebViewController.enableDebugging(true);
+    (controller.platform as AndroidWebViewController)
+        .setMediaPlaybackRequiresUserGesture(false);
+  }
 
   return controller;
 }
 
 Future<void> uLoadWebview(WebViewController controller) async {
-
   final htmlContent = await rootBundle.loadString('assets/index.html');
   controller.loadHtmlString(htmlContent);
 }
